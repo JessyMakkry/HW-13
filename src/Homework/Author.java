a1ec1bb5e1823a6891422fc0b89d293f1e7015ca
JessyMakkry <jessy.makkry@bk.ru>
2024-07-21 15:01:36 +0300

    Commit 1

--- src/Homework/Author.java
+++ src/Homework/Author.java
@@ -1,4 +1,41 @@
 package Homework;
 
 public class Author {
+    private String name;//
+    private String surname;
+    public Author (String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    public String getAuthorName() {
+        return this.name;
+    }
+    public String getAuthorSurname() {
+        return this.surname;
+    }
+    @Override
+    public String toString() {
+        return name + " " + surname;
+    }
+    @Override
+    public boolean equals(Object other){
+        if (this == other) {
+            return true;
+        }
+        if (other == null || this.getClass() != other.getClass()) {
+            return false;
+        }
+        Author a2 = (Author) other;
+        if (this.name.equals(a2.name)&&this.surname.equals(a2.surname)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(surname);
+    }
 }
+
--- src/Homework/Main.java
+++ src/Homework/Main.java
@@ -1,7 +1,35 @@
 package Homework;
 
 public class Main {
+    public static void printseparator(){
+        System.out.println("_____________________________________");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Task 1");
+        Author Pushkin = new Author("Александр", "Пушкин");
+        Author Tolstoy = new Author("Лев", "Толстой");
+        Book theRedOne = new Book("Капитанская дочка", Pushkin, 1836);
+        Book theGreenOne = new Book("Война и мир", Tolstoy,1863);
+        System.out.println("Pushkin = " + Pushkin);
+        System.out.println("Tolstoy = " + Tolstoy);
+        printseparator();
+        System.out.println("theRedOne = " + theRedOne);
+        System.out.println("theGreenOne = " + theGreenOne);
+        printseparator();
+        Author Pushkin2 = new Author("Александр", "Пушкин");
+        System.out.println("Pushkin.equals(Pushkin2) = " + Pushkin.equals(Pushkin2));
+        System.out.println("Pushkin.equals(Tolstoy) = " + Pushkin.equals(Tolstoy));
+        printseparator();
+        Book theBlueOne = new Book ("Капитанская дочка", Pushkin, 1836);
+        System.out.println("theRedOne.equals(theBlueOne) = " + theRedOne.equals(theBlueOne));
+        System.out.println("theRedOne.equals(theGreenOne) = " + theRedOne.equals(theGreenOne));
+        printseparator();
+        System.out.println("Pushkin.hashCode() = " + Pushkin.hashCode());
+        System.out.println("Pushkin2.hashCode() = " + Pushkin2.hashCode());
+        System.out.println("Tolstoy.hashCode() = " + Tolstoy.hashCode());
+        System.out.println("theBlueOne.hashCode() = " + theBlueOne.hashCode());
+        System.out.println("theGreenOne.hashCode() = " + theGreenOne.hashCode());
+        System.out.println("theRedOne.hashCode() = " + theRedOne.hashCode());
+        printseparator();
     }
 }
